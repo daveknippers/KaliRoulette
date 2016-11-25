7279205bd9cc2bbd3cbf5ba30208a3b2d972a441
dave knippers <davek@intmod.com>
2016-11-25 16:33:29 -0700

    changed all tabbing to tab characters

--- i_hate_snakes/i_hate_snakes.py
+++ i_hate_snakes/i_hate_snakes.py
@@ -8,15 +8,15 @@ def i_hate_snakes():
 
 	sp = Spelunker()
 
-    with open('oauth_token', 'r') as fp:
-        password = fp.read()
+	with open('oauth_token', 'r') as fp:
+		password = fp.read()
 
-    channel = '#bunny_funeral'
-    nickname = 'spelunkybot'
-    server = irc.bot.ServerSpec('irc.twitch.tv', port=6667, password=password.strip())
+	channel = '#bunny_funeral'
+	nickname = 'spelunkybot'
+	server = irc.bot.ServerSpec('irc.twitch.tv', port=6667, password=password.strip())
 
-    bot = BetBot(channel, nickname, server)
-    bot.start()
+	bot = BetBot(channel, nickname, server)
+	bot.start()
 
 
 if __name__ == "__main__":
