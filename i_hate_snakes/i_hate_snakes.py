b5b04ac70f84b5c3d3e778901c8f54c006d1dbbc
dave knippers <dave.knippers@gmail.com>
2016-11-25 13:13:32 -0700

    boilerplate code for i_hate_snakes and a small change to make Spelunkybot look for its name inwards.

--- i_hate_snakes/i_hate_snakes.py
+++ i_hate_snakes/i_hate_snakes.py
@@ -1,8 +1,22 @@
 
+from SpelunkyBot import BetBot
+from memory import Spelunker
+
+
 
 def i_hate_snakes():
 
+	sp = Spelunker()
 
+    with open('oauth_token', 'r') as fp:
+        password = fp.read()
+
+    channel = '#bunny_funeral'
+    nickname = 'spelunkybot'
+    server = irc.bot.ServerSpec('irc.twitch.tv', port=6667, password=password.strip())
+
+    bot = BetBot(channel, nickname, server)
+    bot.start()
 
 
 if __name__ == "__main__":
