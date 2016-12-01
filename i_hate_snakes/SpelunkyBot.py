8a2992c571f768ed8e0cebf5172875cd92baefdf
Rellim7 <justin.miller@miraclechannel.ca>
2016-11-30 23:47:22 -0700

    typo

--- i_hate_snakes/SpelunkyBot.py
+++ i_hate_snakes/SpelunkyBot.py
@@ -80,7 +80,7 @@ class BetBot(irc.bot.SingleServerIRCBot):
 			elif (len(cmd)== 4):
 				condition1 = str(cmd[2])
 				condition2 = str(cmd[3])
-				if condition1 == condtion2:
+				if condition1 == condition2:
 					condtion2 = None
 				try:
 					bet = int(cmd[1])
