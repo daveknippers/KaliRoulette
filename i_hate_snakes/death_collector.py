2800ec96fc75bdf3bd8e654d70abfd4a4b7b0c9d
dave willem knippers <dave.knippers@gmail.com>
2016-11-30 22:27:45 -0700

    fixed up death collector

--- i_hate_snakes/death_collector.py
+++ i_hate_snakes/death_collector.py
@@ -8,7 +8,7 @@ import sqlite3
 
 pd.set_option('display.max_columns', None)
 
-# to-do: make this part of Spelunker
+# to-do: make this part of Spelunker, it's ugly here.
 ALL_ATTRIBUTES = ['level',
 			'is_dead',
 			'killed_by',
@@ -45,7 +45,7 @@ ALL_ATTRIBUTES = ['level',
 			'has_crysknife',
 			'has_vlads_amulet']
 
-def main(db_file='death_collection.db'):
+def death_collector(db_file='death_collection.db'):
 	
 	sp = Spelunker()
 	
@@ -53,8 +53,8 @@ def main(db_file='death_collection.db'):
 	run_columns = ['start_time']
 	run_columns.extend(ALL_ATTRIBUTES)
 
-		
 	last_level = 0
+	shopkeeper_angry = 0
 
 	# it should never be read before it gets set in the while below,
 	# but in case it is because i missed a race condition or something,
@@ -62,49 +62,67 @@ def main(db_file='death_collection.db'):
 	start_time = int(time.time()) 
 
 	current_run = []
+	informed = False
 
-	if not sp.is_dead:
-		raise RuntimeError('Please make sure the script is started from a \'dead\' state (ie. on the game over screen)')
-		
-	while True:
-		current_level = sp.level
-		dead = sp.is_dead
-		if dead and last_level:
-			# first time death notification
-			state = [start_time]
-			state.extend(list(map(lambda attr: getattr(sp,attr),ALL_ATTRIBUTES)))
-			current_run.append(state)
-			dc_df = pd.DataFrame(current_run,columns=run_columns)
-			dc_df.to_sql('run_states',sql_engine,if_exists='append',index=False)
-			last_level = 0
-			current_run = []
+	while not sp.is_dead:
+		if not informed:
+			print('Waiting until you\'re dead to start data collection.\n')
+			informed = True
+		time.sleep(1)
 
-			print('You have died. This is what\'s being saved in the database:')
-			print(dc_df)
+	print('Game over state detected. Death collection will begin next life.\n')
 
-		elif not dead and not last_level:
-			# just started a new game
-			start_time = int(time.time())
-			state = [start_time]
-			state.extend(list(map(lambda attr: getattr(sp,attr),ALL_ATTRIBUTES)))
-			current_run.append(state)
-			last_level = current_level
-			print('New game started')
-		elif not dead and current_level > last_level:
-			# just got to a new level
-			last_level = current_level
-			state = [start_time]
-			state.extend(list(map(lambda attr: getattr(sp,attr),ALL_ATTRIBUTES)))
-			current_run.append(state)
-			print('Finished level.')
+	try:
+		while True:
+			current_level = sp.level
+			dead = sp.is_dead
+			timer = sp.level_timer
+			angry_keeper = sp.angry_shopkeeper_1 or sp.angry_shopkeeper_2
+			if dead and last_level:
+				# first time death notification
+				state = [start_time]
+				state.extend(list(map(lambda attr: getattr(sp,attr),ALL_ATTRIBUTES)))
+				current_run.append(state)
+				dc_df = pd.DataFrame(current_run,columns=run_columns)
+				dc_df.to_sql('run_states',sql_engine,if_exists='append',index=False)
+				last_level = 0
+				current_run = []
 
+				print('You have died. This is what\'s being saved in the database:')
+				print(dc_df)
 
-	
-
-		time.sleep(5)
+			elif not dead and not last_level and current_level > 0:
+				# just started a new game
+				start_time = int(time.time())
+				shopkeeper_angry = False
+				if current_level % 4: # deal with shortcuts
+					state = [start_time]
+					state.extend(list(map(lambda attr: getattr(sp,attr),ALL_ATTRIBUTES)))
+					current_run.append(state)
+				last_level = current_level
+				print('New game started')
+			elif not dead and current_level > last_level and timer > 0:
+				# just got to a new level
+				last_level = current_level
+				state = [start_time]
+				state.extend(list(map(lambda attr: getattr(sp,attr),ALL_ATTRIBUTES)))
+				current_run.append(state)
+				print('Finished level / starting from shortcut')
+			if not shopkeeper_angry and angry_keeper:
+				shopkeeper_angry = True
+				state = [start_time]
+				state.extend(list(map(lambda attr: getattr(sp,attr),ALL_ATTRIBUTES)))
+				current_run.append(state)
+				print('Shopkeeper is kinda pissed...')
+			
+			time.sleep(1)
+	except ValueError:
+		print('\nSpelunky data collection stopped.\n')
+	except AttributeError:
+		print('\nSpelunky data collection stopped.\n')
 
 	
 
 if __name__ == "__main__":
-	main()
+	death_collector()
 
