18d66bc0498bb35f654cc4186b38f2bd8710f9b8
dave knippers <davek@intmod.com>
2017-02-22 12:39:33 -0700

    trying to add tk logger through multiprocessing

--- KaliRoulette.py
+++ KaliRoulette.py
@@ -6,12 +6,15 @@ import numpy as np
 import multiprocessing as mp
 
 
-import time, sqlite3, cmd
+import time, sqlite3, cmd, logging
 
 try:
 	import readline
 except ImportError:
 	import pyreadline as readline
+	
+import tkinter as tk
+import tkinter.scrolledtext as ScrolledText
 
 pd.set_option('display.max_columns', None)
 
@@ -59,7 +62,7 @@ class KaliState(mp.Process):
 		state = [self.start_time,self.seq,PLAYER_NAME]
 		state.extend(list(map(lambda attr: getattr(self.sp,attr),Spelunker.ALL_ATTRIBUTES)))
 		self.seq += 1
-		print(state)
+		logging.info(state)
 		self.state_queue.put(state)
 
 	def evaluate_state(self):
@@ -73,20 +76,20 @@ class KaliState(mp.Process):
 			in_mothership = sp.lvl_mothership
 			angry_keeper = sp.angry_shopkeeper
 
-			print(self.last_level,self.seq)
+			logging.info(self.last_level,self.seq)
 
 			if dead:
 				if self.last_level:
 					# first time death notification
 					self.last_level = 0
 					self.announce()
-					print('In KaliState process, You have died')
+					logging.info('In KaliState process, You have died')
 			else:
 				if not current_level and (self.last_level == 16 or self.last_level == 20):
 					# we just won!
 					self.last_level = 0
 					self.announce()
-					print('In KaliState process, We just won!')
+					logging.info('In KaliState process, We just won!')
 				elif not self.last_level and current_level >= 0 and self.seq > 1:
 					# just started a new game
 					self.start_time = int(time.time())
@@ -94,45 +97,45 @@ class KaliState(mp.Process):
 					self.mothership_trigger = False
 					self.seq = 1
 					self.last_level = current_level
-					print('In KaliState process, cleansing our state')
+					logging.info('In KaliState process, cleansing our state')
 					if current_level % 4 == 1: # deal with shortcuts
 						self.announce()
-						print('In KaliState process, New game started')
+						logging.info('In KaliState process, New game started')
 					
 				elif current_level > self.last_level:
 					# just got to a new level
 					self.last_level = current_level
 					if self.seq == 1 and current_level % 4 == 1:
-						print('In KaliState process, New game started')
+						logging.info('In KaliState process, New game started')
 						self.announce()
 					elif self.seq == 1:
 						pass
 					else:
-						print('In KaliState process, Level Finished')
+						logging.info('In KaliState process, Level Finished')
 						self.announce()
 				elif in_mothership and current_level == 11 and not self.mothership_trigger:
 					# entered the mothership, special level processing
 					self.mothership_trigger = True
 					self.announce()
-					print('In KaliState process, entered the mothership, repeating 3-3.')
+					logging.info('In KaliState process, entered the mothership, repeating 3-3.')
 					
 				elif not in_mothership and current_level == 12 and self.mothership_trigger:
 					# finished the mothership, special level processing
 					self.mothership_trigger = False
 					self.announce()
-					print('In KaliState process, exited the mothership, repeating 3-4')
+					logging.info('In KaliState process, exited the mothership, repeating 3-4')
 					
 				elif not self.angry_shopkeeper_trigger and angry_keeper:
 					self.angry_shopkeeper_trigger = True
 					self.announce()
-					print('In KaliState process, shopkeeper is kinda pissed...')
+					logging.info('In KaliState process, shopkeeper is kinda pissed...')
 
 			self.last_timer = timer
 
 		except ValueError:
-			print('\nSpelunky data collection stopped.\n')
+			logging.info('\nSpelunky data collection stopped.\n')
 		except AttributeError:
-			print('\nSpelunky data collection stopped.\n')
+			logging.info('\nSpelunky data collection stopped.\n')
 
 class KaliWhisper(cmd.Cmd):
 	intro = 'Welcome to the KaliRoulette shell. Type help or ? to list commands.\n'
@@ -151,11 +154,54 @@ class KaliWhisper(cmd.Cmd):
 		if self.game_state:
 			print('init already called')
 		else:
+		
+			self.logging_process = mp.Process(target=WindowLogging, args=())
 			self.game_state = KaliState(self.state_queue)
 			self.game_state.start()
 			self.oracle = Oracle(self.state_queue)
 			self.oracle.start()
 
+class TKLogging(tk.Frame):
+	'''http://stackoverflow.com/questions/13318742/python-logging-to-tkinter-text-widget'''
+
+	def __init__(self, parent, *args, **kwargs):
+			tk.Frame.__init__(self, parent, *args, **kwargs)
+			self.root = parent
+			self.build_gui()
+			
+    def build_gui(self):                    
+        # Build GUI
+        self.root.title('TEST')
+        self.root.option_add('*tearOff', 'FALSE')
+        self.grid(column=0, row=0, sticky='ew')
+        self.grid_columnconfigure(0, weight=1, uniform='a')
+        self.grid_columnconfigure(1, weight=1, uniform='a')
+        self.grid_columnconfigure(2, weight=1, uniform='a')
+        self.grid_columnconfigure(3, weight=1, uniform='a')
+
+        # Add text widget to display logging info
+        st = ScrolledText.ScrolledText(self, state='disabled')
+        st.configure(font='TkFixedFont')
+        st.grid(column=0, row=1, sticky='w', columnspan=4)
+
+        # Create textLogger
+        text_handler = TextHandler(st)
+
+        # Logging configuration
+        logging.basicConfig(filename='test.log',
+            level=logging.INFO, 
+            format='%(asctime)s - %(levelname)s - %(message)s')        
+
+        # Add the handler to logger
+        logger = logging.getLogger()        
+        logger.addHandler(text_handler)
+	
+def WindowLogging():
+	root = tk.Tk()
+	tk_logging = TKLogging(root)
+	root.mainloop()
+
+			
 if __name__ == "__main__":
 	KaliWhisper().cmdloop()
 
