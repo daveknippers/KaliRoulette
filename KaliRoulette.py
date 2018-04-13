7866ba4b399138f159747e426a5dff1c2801ed68
dave KNIPPERS <dave.knippers@gmail.com>
2018-04-12 18:16:08 -0600

    GETTIN' THERE

--- KaliRoulette.py
+++ KaliRoulette.py
@@ -29,6 +29,8 @@ class KaliBot(irc.bot.SingleServerIRCBot):
 		self.is_dead = self.sp.is_dead
 		self.angry_shopkeeper = self.sp.angry_shopkeeper
 		self.has_ankh  = self.sp.has_ankh
+		
+		self.show_pause = True
 
 		self.lock = Lock()
 
@@ -53,9 +55,13 @@ class KaliBot(irc.bot.SingleServerIRCBot):
 			if self.timer == current_timer:
 				if current_level == 0 and (self.level == 16 or self.level == 20):
 					self.send_pub_message('Streamer has won!')
-				elif self.enable_bets == False:
-					self.send_pub_message('Game paused, betting available for next 30 seconds')
+				elif self.enable_bets == False and self.show_pause:
+					self.send_pub_message('Game paused, betting is available for the next 30 seconds.')
 					self.enable_bets = True
+					self.show_pause = False
+					Timer(30,self.end_betting).start()
+			else:
+				self.show_pause = True
 
 		
 		self.timer = current_timer
@@ -66,22 +72,28 @@ class KaliBot(irc.bot.SingleServerIRCBot):
 		
 		self.lock.release()
 
+	def end_betting(self):
+		self.lock.acquire()
+
+
+		self.send_pub_message('Betting has ended.')
+		self.enable_bets = False
+
+		self.lock.release()
+
 	def send_message(self, message, user):
 		self.connection.privmsg(self.channel,'/w {} {}'.format(user,message))
 		
 	def send_pub_message(self, message):
-		#self.lock.acquire()
 		self.connection.privmsg(self.channel, message)
 			
 	def on_pubmsg(self, c, e):
-		print(c,e)
-		#a = e.arguments[0].split("!", 1)
-		#if (len(a) > 1):
-		#	print(e, a[1].strip(),a)
+		msg = e.arguments[0].split("!", 1)[1:][0]
+		if len(msg) > 0:
+			self.do_command(msg,e)
 
 	def on_whisper(self, c, e):
 		self.on_pubmsg(c,e)
-
 
 	def on_welcome(self, c, e):
 		print('joining {}'.format(self.channel))
@@ -89,19 +101,42 @@ class KaliBot(irc.bot.SingleServerIRCBot):
 		c.join(self.channel)
 		c.set_rate_limit(100/30)
 
+	def do_command(self,msg,meta):
+		user = meta.source.split("!")[0]
+		msg = msg.strip().split()
+		if len(msg) == 0:
+			return
 
-				   
-def KaliRoulette(streamer_name):
+		command = msg[0]
+
+		if command == 'help':
+			self.send_message('Available commands: !bet amount death_cause, !balance',user)
+
+		if command == 'bet':
+			self.lock.acquire()
+			if self.enable_bets:
+				if len(msg) != 3:
+					reason = "Invalid betting parameters. Try !bet amount death_cause. The causes can be found on bunny_funeral's channel panels.")
+					self.send_message(user,reason)
+				else:
+
+
+			else:
+				self.send_message('Betting is currently disallowed. Please wait for the game to pause.',user)
+			self.lock.release()
+
+def KaliRoulette(streamer_name, bot_name):
 
 	server = irc.bot.ServerSpec('irc.twitch.tv', port=6667, password=oauth_token)
-	bot = KaliBot(server, '#{}'.format(streamer_name), 'SpelunkyBot')
+	bot = KaliBot(server, '#{}'.format(streamer_name), bot_name)
 	bot.start()
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='Start the Kali Roulette memory reader/IRC bot')
 	parser.add_argument('-s', '--streamer', dest='streamer', type=str, help='your twitch username')
+	parser.add_argument('-b', '--bot-name', dest='bot_name', type=str, help="your bot's twitch username")
 	args = parser.parse_args()
-	if args.streamer is None:
+	if args.streamer is None or args.bot_name is None:
 		parser.print_help()
 	else:
 		KaliRoulette(args.streamer)
